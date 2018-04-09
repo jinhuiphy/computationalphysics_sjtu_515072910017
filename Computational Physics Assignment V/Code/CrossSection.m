45a470789daaf3759bab29ed3a015a206ca8c138
wuli_zha <lijinhui1997@hotmail.com>
2018-04-09 22:00:23 +0800

    update assignV problem1

--- Computational Physics Assignment V/Code/CrossSection.m
+++ Computational Physics Assignment V/Code/CrossSection.m
@@ -1,15 +1,37 @@
-% E = 0:25:200;
-% f = [10.6 16.0 45 83.5 52.8 19.9 10.8 8.25 4.7];
-% Ei = [10 90 185];
-% 
-% for i = 1:length(Ei)
+% % CodeName: CrossSection.m
+% % This code is used to predict the f(E) for a given E 
+% % and choose a suitable boundary condition.
 
-h = [0.1 0.01 0.001 0.0001];
-
-for i = 1:4
-    digits(8);
-    y1 = vpa(cos(1+ h(i)));
-    y_1 = vpa(cos(1-h(i)));
-    y = vpa((y1- y_1)./(2.*h(i)));
-    fprintf('y1 = %.8f, y_1 = %.8f, y = %.8f\n', y1, y_1, y);
+% 初始化参数
+E = 0:25:200;
+f = [10.6 16.0 45 83.5 52.8 19.9 10.8 8.25 4.7];
+delta = [9.34 17.9 41.5 85.5 51.5 21.5 10.8 6.29 4.14];
+Ei = [10 90 185];
+step = 0.1;
+% 作三次样条插值，Free Boundary Condition
+E_spline = 0:step:200;
+f_spline = spline(E, f, E_spline);
+% 寻找最大值对应的位置
+index = find(f_spline == max(max(f_spline1)));
+fprintf('Er = %f\n', E_spline(index));
+half_max = f_spline(index) ./ 2;
+% 找到f(E)为半高对应的E
+for i = 1:length(E_spline)
+    if abs(f_spline(1, i) - half_max) < step
+        fprintf('E = %f\t', E_spline(i));
+    end
 end
+fprintf('\n\n');
+% 将三次样条插值得到的结果画出来
+plot(E_spline, f_spline, 'r');
+hold on;
+plot(E, f, 'bo');
+legend('Spline', 'Original');
+grid on;
+% 给定E得到插值后的f(E)
+for i = 1:length(Ei)
+    fi = spline(E, f, Ei(i));
+    fprintf('E = %d, f(E) = %f\n', Ei(i), fi);
+end
+
+
