c1d2c828f8b6edfb42050fb67056314b5ce8c68e
wuli_zha <lijinhui1997@hotmail.com>
2018-04-26 01:00:18 +0800

    update code

--- Computational Physics Assignment VII/Code/SolvePro2.m
+++ Computational Physics Assignment VII/Code/SolvePro2.m
@@ -21,18 +21,18 @@ Jacob = jacobian([F1, F2, F3], [a1;a2;a3]);
 
 % 设置精度和初始值，以及最大迭代次数
 Tolerance = 1e-6;
-X(:,1) = [7.1e4; 70; 850];
+X(1,:) = [7.1e4, 70, 850];
 N = 200;
 cnt = 0;
 % 使用Newton-Raphson方法进行迭代
 for i = 1:N
-    J = subs(Jacob, [a1,a2,a3], [X(1,i), X(2,i), X(3,i)]);  % 雅可比矩阵的数值
-    F = subs([F1;F2;F3], [a1,a2,a3], [X(1,i), X(2,i), X(3,i)]); % 函数值的数值
+    J = subs(Jacob, [a1,a2,a3], X(i,:));  % 雅可比矩阵的数值
+    F = subs([F1;F2;F3], [a1,a2,a3], X(i,:)); % 函数值的数值
     d_X = J \ F;    
-    X(:,i+1) = X(:,i) - d_X;
+    X(i+1,:) = X(i,:) - d_X';
     if norm(d_X, inf) < Tolerance
         cnt = i+1;
         break;
     end
 end
-fprintf("After %d interation, a1=%.2f, a2=%.2f, a3=%.2f\n", cnt, X(1, cnt), X(2, cnt), X(3, cnt));
+fprintf("After %d interation, a1=%.2f, a2=%.2f, a3=%.2f\n", cnt, X(cnt, 1), X(cnt, 2), X(cnt, 3));
